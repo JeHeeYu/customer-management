b404114586db7f4b58af9c15d82847d0f2cfce77
JeHeeYu <sees1234558@gmail.com>
2022-09-05 16:22:13 +0900

    Apply Material UI

--- README.md
+++ README.md
@@ -11,6 +11,9 @@ cd Workstation
 // customer-management 라는 React Project 생성
 create-react-app customer-management
 
+// material ui download
+npm install @mui/material @emotion/react @emotion/styled
+
 cd customer-management
 yarn start
 </pre>
--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,49 +1,22 @@
 import React from 'react';
+import TableRow from '@mui/material/TableRow'
+import TableCell from '@mui/material/TableCell'
 
 class Customer extends React.Component {
     // Customer 의 내용을 그려주는 부분
     render() {
         return (
-            // App.js에서 받은 데이터로 출력
-            <div>
-                <CustomerProfile 
-                    id={this.props.id} 
-                    image={this.props.image}
-                    name={this.props.name}
-                />
-                <CustomerInfo
-                    birthday={this.props.birthday} 
-                    gender={this.props.gender}
-                    job={this.props.job}
-                />
-            </div>
+            <TableRow>
+                <TableCell>{this.props.id}</TableCell>
+                <TableCell><img src={this.props.image} alt="profile"/></TableCell>
+                <TableCell>{this.props.name}</TableCell>
+                <TableCell>{this.props.birthday}</TableCell>
+                <TableCell>{this.props.gender}</TableCell>
+                <TableCell>{this.props.job}</TableCell>
+            </TableRow>
         )
     }
 }
 
-// 사용자의 Profile 이름과 ID 값을 출력할 Class
-class CustomerProfile extends React.Component {
-    render() {
-        return (
-            <div>
-                <img src={this.props.image} alt="profile"/>
-                <h2>{this.props.name}({this.props.id})</h2>
-            </div>
-        )
-    }
-}
-
-// 사용자의 데이터 출력하기 위한 Class
-class CustomerInfo extends React.Component {
-    render() {
-        return (
-            <div>
-                <p>{this.props.birthday}</p>
-                <p>{this.props.gender}</p>
-                <p>{this.props.job}</p>
-            </div>
-        )
-    }
-}
 
 export default Customer;
