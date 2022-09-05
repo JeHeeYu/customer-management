4cb34f4a6e972fd68a5d3f0c2bedfce9a4e5368c
JeHeeYu <sees1234558@gmail.com>
2022-09-05 14:52:19 +0900

    Create Project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="gray-background">
+        <h2>React Project Create</h2>
+    </div>
+  );
+}
+
+export default App;
