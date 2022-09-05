3379c47f02a24dcfaebbc9c511c8f9acc9cbaabe
JeHeeYu <sees1234558@gmail.com>
2022-09-05 15:43:02 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -2,23 +2,54 @@ import React, { Component } from 'react';
 import Customer from './components/Customer'
 import './App.css';
 
-const customer = {
-    'name': '홍길동',
-    'birthday': '960929',
-    'gender': '남자',
-    'job': '대학생'
+const customers = [
+    {
+    'id': 1,
+    'image': 'https://placeimg.com/64/64/any/1',
+    'name': '홍길동1',
+    'birthday': '9609291',
+    'gender': '남자1',
+    'job': '대학생1'
+},
+{
+    'id': 2,
+    'image': 'https://placeimg.com/64/64/any/2',
+    'name': '홍길동2',
+    'birthday': '9609292',
+    'gender': '남자2',
+    'job': '대학생2'
+},
+{
+    'id': 3,
+    'image': 'https://placeimg.com/64/64/any/3',
+    'name': '홍길동3',
+    'birthday': '9609293',
+    'gender': '남자3',
+    'job': '대학생3'
 }
+]
 
 class App extends Component {
     render() {
         return (
             // Customer 컴포넌트에 데이터 전송
-            <Customer
-                name={customer.name}
-                birthday={customer.birthday}
-                gender={customer.gender}
-                job={customer.job}
-            />
+            <div>
+                {
+                    customers.map(c => {
+                        return (
+                            <Customer
+                                key={c.id}
+                                id={c.id}
+                                image={c.image}
+                                name={c.name}
+                                birthday={c.birthday}
+                                gender={c.gender}
+                                job={c.job}
+                            />
+                        )
+                    })
+                }
+            </div>
         );
     }
 }
--- src/components/Customer.js
+++ src/components/Customer.js
@@ -6,7 +6,38 @@ class Customer extends React.Component {
         return (
             // App.js에서 받은 데이터로 출력
             <div>
-                <h2>{this.props.name}</h2>
+                <CustomerProfile 
+                    id={this.props.id} 
+                    image={this.props.image}
+                    name={this.props.name}
+                />
+                <CustomerInfo
+                    birthday={this.props.birthday} 
+                    gender={this.props.gender}
+                    job={this.props.job}
+                />
+            </div>
+        )
+    }
+}
+
+// 사용자의 Profile 이름과 ID 값을 출력할 Class
+class CustomerProfile extends React.Component {
+    render() {
+        return (
+            <div>
+                <img src={this.props.image} alt="profile"/>
+                <h2>{this.props.name}({this.props.id})</h2>
+            </div>
+        )
+    }
+}
+
+// 사용자의 데이터 출력하기 위한 Class
+class CustomerInfo extends React.Component {
+    render() {
+        return (
+            <div>
                 <p>{this.props.birthday}</p>
                 <p>{this.props.gender}</p>
                 <p>{this.props.job}</p>
